7f8a38f07aceeb602ea4146be7fdd319770172f8
Adolfo Santiago <epoch@nixnetmail.com>
2023-03-31 08:59:25 +0200

    Fix CharactersViewModelTest

--- app/src/test/java/com/adolfo/marvel/features/viewmodels/CharactersViewModelTest.kt
+++ app/src/test/java/com/adolfo/marvel/features/viewmodels/CharactersViewModelTest.kt
@@ -4,21 +4,18 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import androidx.lifecycle.SavedStateHandle
 import com.adolfo.characters.data.models.view.CharactersView
-import com.adolfo.characters.domain.repository.CharactersRepository
 import com.adolfo.characters.domain.usecases.GetCharacters
-import com.adolfo.core.functional.State
 import com.adolfo.core.functional.State.Success
 import com.adolfo.core_testing.CoroutineTestRule
 import com.adolfo.marvel.features.character.view.viewmodel.CharactersViewModel
-import io.mockk.coEvery
-import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.unmockkAll
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.consumeAsFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,47 +24,38 @@ import org.junit.Test
 class CharactersViewModelTest {
 
     @get:Rule
-    var coroutinesRule = CoroutineTestRule()
+    val coroutineTestRule = CoroutineTestRule()
 
     @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
+    val instantExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var viewModel: CharactersViewModel
-
-    private var repository = mockk<CharactersRepository>()
-    private var getCharacters = mockk<GetCharacters>()
-
+    private val getCharacters = mockk<GetCharacters>()
     private val charactersObserver = mockk<Observer<CharactersView>>()
 
     @Before
     fun setup() {
-        getCharacters = GetCharacters(repository)
+        viewModel = CharactersViewModel(SavedStateHandle(), getCharacters)
+    }
 
-        viewModel = CharactersViewModel(SavedStateHandle(), getCharacters).apply {
-            characters.observeForever(charactersObserver)
-        }
+    @After
+    fun tearDown() {
+        unmockkAll()
     }
 
     @Test
     fun `should emit get characters`() = runTest {
-        val expectedResult = Success(
-            CharactersView(listOf(), isFullEmpty = true)
-        )
+        viewModel.characters.observeForever(charactersObserver)
+        val expectedResult = Success(CharactersView(listOf(), isFullEmpty = true))
 
-        val channel = Channel<State<CharactersView>>()
-        val flow = channel.consumeAsFlow()
-
-        coEvery {
-            repository.getCharacters(0, false, 15)
+        val flow = MutableStateFlow(expectedResult)
+        every {
+            getCharacters(GetCharacters.Params(0, false))
         } returns flow
-
-        launch {
-            channel.send(expectedResult)
-        }
 
         viewModel.getCharacters(false)
 
-        coVerify {
+        verify {
             charactersObserver.onChanged(expectedResult.data)
         }
     }
