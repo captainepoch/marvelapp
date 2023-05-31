91e2342748edd2cff126d669bb6d05799d0feceb
Adolfo Santiago <epoch@nixnetmail.com>
2023-05-31 18:25:33 +0200

    Fix GetCharacterTest

--- characters/src/test/java/com/adolfo/characters/usecases/GetCharacterTest.kt
+++ characters/src/test/java/com/adolfo/characters/usecases/GetCharacterTest.kt
@@ -2,50 +2,41 @@ package com.adolfo.characters.usecases
 
 import com.adolfo.characters.data.datasource.CharactersDatasourceImp
 import com.adolfo.characters.data.models.entity.CharacterEntity
-import com.adolfo.characters.data.models.entity.CharactersEntity
 import com.adolfo.characters.data.models.view.CharacterView
 import com.adolfo.characters.domain.repository.CharactersRepository
 import com.adolfo.characters.domain.repository.CharactersRepositoryImp
-import com.adolfo.characters.domain.usecases.GetCharacterDetail
 import com.adolfo.core.functional.State.Success
-import com.nhaarman.mockitokotlin2.doReturn
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.amshove.kluent.`should be instance of`
 import org.amshove.kluent.shouldBeEqualTo
-import org.junit.Rule
 import org.junit.Test
-import org.koin.test.mock.MockProviderRule
-import org.mockito.Mockito
-import org.mockito.Mockito.mock
 
-class GetCharacterTest : UnitTest() {
+class GetCharacterTest {
 
     private lateinit var repository: CharactersRepository
-    private lateinit var getCharacters: GetCharacterDetail
-
-    @get:Rule
-    val mockProvider = MockProviderRule.create { clazz ->
-        Mockito.mock(clazz.java)
-    }
 
     @Test
     fun `should get characters`() = runBlocking {
-        val mockResponse = Success(CharacterEntity.empty().toCharacter().toCharacterView())
+        val mockResponse = Success(CharacterEntity.empty())
 
-        val dataSource = mock<CharactersDatasourceImp> {
-            onBlocking { getCharacter(0) } doReturn mockResponse
-        }
+        val dataSource = mockk<CharactersDatasourceImp>()
+        coEvery {
+            dataSource.getCharacter(0)
+        } returns mockResponse
 
         repository = CharactersRepositoryImp(dataSource)
-        getCharacters = GetCharacterDetail(repository)
 
         val flow = repository.getCharacter(0)
         flow.collect { result ->
-            result.`should be instance of`<Success<CharactersEntity>>()
+            result.`should be instance of`<Success<CharacterView>>()
             when (result) {
                 is Success<CharacterView> -> {
-                    result.data shouldBeEqualTo mockResponse.data
+                    result.data shouldBeEqualTo mockResponse.data.toCharacter().toCharacterView()
                 }
+
+                else -> {}
             }
         }
     }
