188707c288f0a5cd4fb5f18de623eca26b788a34
Adolfo Santiago <epoch@nixnetmail.com>
2023-06-01 18:03:33 +0200

    Save List state and scroll loading more items

--- app/src/main/java/com/adolfo/marvel/common/navigation/CharactersScreen.kt
+++ app/src/main/java/com/adolfo/marvel/common/navigation/CharactersScreen.kt
@@ -5,7 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,12 +23,22 @@ fun CharactersScreen(
     modifier: Modifier = Modifier,
     viewModel: CharactersViewModelCompose = koinViewModel()
 ) {
+    val listState = rememberLazyListState()
+    val isAtBottom by remember { derivedStateOf { !listState.canScrollForward } }
+
+    LaunchedEffect(key1 = isAtBottom) {
+        if (isAtBottom) {
+            viewModel.getCharacters(true)
+        }
+    }
+
     Column(modifier = modifier) {
         LazyColumn(
+            state = listState,
             verticalArrangement = Arrangement.spacedBy(8.dp),
             contentPadding = PaddingValues(8.dp)
         ) {
-            items(viewModel.characters.value.characters) { hero ->
+            items(viewModel.characters.value.characters, key = { it.id }) { hero ->
                 CharacterScreenItem(modifier, hero = hero)
             }
         }
