e244f886f212afd606331b3d148acb7157562fa6
Adolfo Santiago <epoch@nixnetmail.com>
2023-06-02 16:53:25 +0200

    Add Scaffold with a top appbar

--- app/src/main/java/com/adolfo/marvel/common/navigation/CharactersScreen.kt
+++ app/src/main/java/com/adolfo/marvel/common/navigation/CharactersScreen.kt
@@ -3,9 +3,14 @@ package com.adolfo.marvel.common.navigation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -13,7 +18,9 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.adolfo.marvel.R
 import com.adolfo.marvel.common.navigation.models.CharacterScreenItem
 import com.adolfo.marvel.features.character.view.viewmodel.CharactersViewModelCompose
 
@@ -33,15 +40,28 @@ fun CharactersScreen(
         }
     }
 
-    Column(modifier = modifier) {
-        LazyColumn(
-            state = listState,
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(8.dp)
-        ) {
-            items(state.characters, key = { it.id }) { hero ->
-                CharacterScreenItem(modifier, hero = hero) {
-                    onCharacterClicked(hero.id)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.characters_toolbar_title),
+                        style = MaterialTheme.typography.h6
+                    )
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(modifier = modifier.padding(paddingValues)) {
+            LazyColumn(
+                state = listState,
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                items(state.characters, key = { it.id }) { hero ->
+                    CharacterScreenItem(modifier, hero = hero) {
+                        onCharacterClicked(hero.id)
+                    }
                 }
             }
         }
