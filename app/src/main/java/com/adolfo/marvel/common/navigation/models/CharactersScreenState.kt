04d57ba7a639b2310277f6ffbe6d3f0f71942517
Adolfo Santiago <epoch@nixnetmail.com>
2023-06-03 18:58:41 +0200

    Loading by default

--- app/src/main/java/com/adolfo/marvel/common/navigation/models/CharactersScreenState.kt
+++ app/src/main/java/com/adolfo/marvel/common/navigation/models/CharactersScreenState.kt
@@ -3,7 +3,7 @@ package com.adolfo.marvel.common.navigation.models
 import com.adolfo.core.functional.State.Error
 
 data class CharactersScreenState(
-    val isLoading: Boolean = false,
+    val isLoading: Boolean = true,
     val error: Error? = null,
-    val characters: List<CharacterItemModel> = emptyList()
+    val characters: List<CharacterItemModel> = listOf()
 )
