07ae8624b5d412bdebee1809feef0d1cd429d0e8
Adolfo Santiago <epoch@nixnetmail.com>
2023-06-04 12:57:41 +0200

    CustomError handled with Snackbar

--- app/src/main/java/com/adolfo/marvel/common/navigation/CharactersScreen.kt
+++ app/src/main/java/com/adolfo/marvel/common/navigation/CharactersScreen.kt
@@ -1,6 +1,5 @@
 package com.adolfo.marvel.common.navigation
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Card
@@ -15,12 +15,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -29,13 +31,15 @@ import com.adolfo.core.exception.Failure.NetworkConnection
 import com.adolfo.core.exception.Failure.ServerError
 import com.adolfo.marvel.R.drawable
 import com.adolfo.marvel.R.string
+import com.adolfo.marvel.common.navigation.models.CharacterItemModel
 import com.adolfo.marvel.common.navigation.models.CharacterScreenItem
+import com.adolfo.marvel.common.navigation.models.CharactersScreenState.LoadingType
 import com.adolfo.marvel.common.navigation.models.CharactersScreenState.LoadingType.NORMAL
 import com.adolfo.marvel.common.navigation.models.CharactersScreenState.LoadingType.PAGINATION
 import com.adolfo.marvel.features.character.view.viewmodel.CharactersViewModelCompose
 import kotlin.system.exitProcess
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun CharactersScreen(
     modifier: Modifier = Modifier,
@@ -44,6 +48,8 @@ fun CharactersScreen(
 ) {
     val state by viewModel.characters.collectAsState()
     val listState = rememberLazyListState()
+    val scaffoldState = rememberScaffoldState()
+    val snackbarCoroutineScope = rememberCoroutineScope()
     val isAtBottom by remember {
         derivedStateOf { !listState.canScrollForward && listState.layoutInfo.totalItemsCount > 0 }
     }
@@ -54,8 +60,22 @@ fun CharactersScreen(
         }
     }
 
+    LaunchedEffect(key1 = state.error?.failure) {
+        val failure = state.error?.failure
+        if (failure is CustomError) {
+            when (failure.code) {
+                CustomError.PAGINATION_ERROR -> {
+                    snackbarCoroutineScope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar("Button Clicked")
+                    }
+                }
+            }
+        }
+    }
+
     if (state.isLoading != NORMAL) {
         Scaffold(
+            scaffoldState = scaffoldState,
             topBar = {
                 TopAppBar(
                     title = {
@@ -80,7 +100,14 @@ fun CharactersScreen(
                     )
 
                     is CustomError -> {
-                        // TODO: Snackbar if pagination
+                        CharactersList(
+                            modifier = modifier,
+                            paddingValues = paddingValues,
+                            listState = listState,
+                            characters = state.characters,
+                            isLoading = state.isLoading,
+                            onCharacterClicked = onCharacterClicked
+                        )
                     }
 
                     else -> GenericErrorView(
@@ -100,39 +127,58 @@ fun CharactersScreen(
                         onDecline = { exitProcess(-1) }
                     )
                 } else {
-                    Column(modifier = modifier.padding(paddingValues)) {
-                        LazyColumn(
-                            state = listState,
-                            verticalArrangement = Arrangement.spacedBy(16.dp),
-                            contentPadding = PaddingValues(16.dp)
-                        ) {
-                            items(state.characters, key = { it.id }) { hero ->
-                                CharacterScreenItem(modifier, hero = hero) {
-                                    onCharacterClicked(hero.id)
-                                }
-                            }
-
-                            item(key = state.isLoading) {
-                                when (state.isLoading) {
-                                    PAGINATION -> {
-                                        Card(
-                                            modifier = modifier
-                                                .fillMaxWidth()
-                                                .height(128.dp)
-                                        ) {
-                                            Loader(modifier)
-                                        }
-                                    }
-
-                                    else -> {}
-                                }
-                            }
-                        }
-                    }
+                    CharactersList(
+                        modifier = modifier,
+                        paddingValues = paddingValues,
+                        listState = listState,
+                        characters = state.characters,
+                        isLoading = state.isLoading,
+                        onCharacterClicked = onCharacterClicked
+                    )
                 }
             }
         }
     } else {
         Loader(modifier)
+    }
+}
+
+@Composable
+fun CharactersList(
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues,
+    listState: LazyListState,
+    characters: List<CharacterItemModel>,
+    isLoading: LoadingType,
+    onCharacterClicked: (Int) -> Unit
+) {
+    Column(modifier = modifier.padding(paddingValues)) {
+        LazyColumn(
+            state = listState,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            items(characters, key = { it.id }) { hero ->
+                CharacterScreenItem(modifier, hero = hero) {
+                    onCharacterClicked(hero.id)
+                }
+            }
+
+            item(key = isLoading) {
+                when (isLoading) {
+                    PAGINATION -> {
+                        Card(
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .height(128.dp)
+                        ) {
+                            Loader(modifier)
+                        }
+                    }
+
+                    else -> {}
+                }
+            }
+        }
     }
 }
