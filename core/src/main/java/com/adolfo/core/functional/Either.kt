dbb5c8f1361ca2d9774be7ef3a683a6859e0b6f6
Adolfo Santiago <epoch@adol.pw>
2023-08-26 16:17:23 +0200

    Modify Either to remove boilerplate generics

--- core/src/main/java/com/adolfo/core/functional/Either.kt
+++ core/src/main/java/com/adolfo/core/functional/Either.kt
@@ -2,20 +2,32 @@ package com.adolfo.core.functional
 
 sealed class Either<out L, out R> {
 
-    data class Left<out L, out R>(val data: L) : Either<L, R>()
-    data class Right<out L, out R>(val data: R) : Either<L, R>()
+    data class Left<L>(val data: L) : Either<L, Nothing>()
+    data class Right<R>(val data: R) : Either<Nothing, R>()
 
-    fun isRight() = this is Right
+    fun isRight() = this is Right<R>
 
-    fun isLeft() = this is Left
+    fun isLeft() = this is Left<L>
 
-    fun asLeftOrNull() = (this as? Left<L, R>)?.data
+    fun asLeftOrNull() = (this as? Left<L>)?.data
 
-    fun asRightOrNull() = (this as? Right<L, R>)?.data
+    fun asRightOrNull() = (this as? Right<R>)?.data
 
-    fun asLeft(): L = (this as Left<L, R>).data
+    fun asLeft(): L = (this as Left<L>).data
 
-    fun asRight(): R = (this as Right<L, R>).data
+    fun asRight(): R = (this as Right<R>).data
+
+    fun <T> fold(funLeft: (L) -> T, funRight: (R) -> T): T = when (this) {
+        is Left -> funLeft(asLeft())
+        is Right -> funRight(asRight())
+    }
+
+    suspend fun <T> coFold(funLeft: suspend (L) -> T, funRight: suspend (R) -> T): T {
+        return when (this) {
+            is Left -> funLeft(asLeft())
+            is Right -> funRight(asRight())
+        }
+    }
 
     inline fun <N> map(crossinline mapper: (R) -> N): Either<L, N> {
         return if (this.isLeft()) {
