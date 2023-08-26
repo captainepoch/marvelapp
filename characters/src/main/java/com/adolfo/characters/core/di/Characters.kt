244561bed423cd2178989631582af58be4c3cebb
Adolfo Santiago <epoch@adol.pw>
2023-08-26 15:54:22 +0200

    Modify Koin modules for characters module

--- characters/src/main/java/com/adolfo/characters/core/di/Characters.kt
+++ characters/src/main/java/com/adolfo/characters/core/di/Characters.kt
@@ -2,32 +2,18 @@ package com.adolfo.characters.core.di
 
 import androidx.room.Room
 import com.adolfo.characters.core.utils.CharactersConstants
-import com.adolfo.characters.data.service.CharactersService
 import com.adolfo.characters.data.database.CharactersDatabase
-import com.adolfo.characters.data.datasource.CharactersDatasource
-import com.adolfo.characters.data.datasource.CharactersDatasourceImp
 import com.adolfo.characters.data.local.CharactersLocal
 import com.adolfo.characters.data.local.CharactersLocalImp
+import com.adolfo.characters.data.service.CharactersApi
+import com.adolfo.characters.data.service.CharactersService
 import com.adolfo.characters.domain.repository.CharactersRepository
 import com.adolfo.characters.domain.repository.CharactersRepositoryImp
 import com.adolfo.characters.domain.usecases.GetCharacterDetail
 import com.adolfo.characters.domain.usecases.GetCharacters
 import org.koin.dsl.module
 
-val charactersRepository = module {
-    factory<CharactersRepository> { CharactersRepositoryImp(get()) }
-}
-
-val charactersService = module {
-    factory { CharactersService(get()) }
-}
-
-val charactersUseCases = module {
-    factory { GetCharacters(get()) }
-    factory { GetCharacterDetail(get()) }
-}
-
-val charactersDatabase = module {
+private val charactersDatabase = module {
     factory {
         Room.databaseBuilder(
             get(),
@@ -39,14 +25,23 @@ val charactersDatabase = module {
     factory<CharactersLocal> { CharactersLocalImp(get()) }
 }
 
-val charactersDatasource = module {
-    factory<CharactersDatasource> { CharactersDatasourceImp(get(), get(), get()) }
+private val charactersRepository = module {
+    factory<CharactersRepository> { CharactersRepositoryImp(get(), get(), get()) }
+}
+
+private val charactersSources = module {
+    factory<CharactersApi> { CharactersService(get()) }
+    factory<CharactersLocal> { CharactersLocalImp(get()) }
+}
+
+private val charactersUseCases = module {
+    factory { GetCharacters(get()) }
+    factory { GetCharacterDetail(get()) }
 }
 
 val characters = listOf(
     charactersDatabase,
-    charactersService,
     charactersRepository,
-    charactersUseCases,
-    charactersDatasource
+    charactersSources,
+    charactersUseCases
 )
