cd556d06fe7097f8fa38ec936e55fff229506747
Adolfo Santiago <epoch@adol.pw>
2023-08-26 15:57:06 +0200

    Rename value to data in Either

--- core/src/main/java/com/adolfo/core/functional/Either.kt
+++ core/src/main/java/com/adolfo/core/functional/Either.kt
@@ -2,20 +2,20 @@ package com.adolfo.core.functional
 
 sealed class Either<out L, out R> {
 
-    data class Left<out L, out R>(val value: L) : Either<L, R>()
-    data class Right<out L, out R>(val value: R) : Either<L, R>()
+    data class Left<out L, out R>(val data: L) : Either<L, R>()
+    data class Right<out L, out R>(val data: R) : Either<L, R>()
 
     fun isRight() = this is Right
 
     fun isLeft() = this is Left
 
-    fun asLeftOrNull() = (this as? Left<L, R>)?.value
+    fun asLeftOrNull() = (this as? Left<L, R>)?.data
 
-    fun asRightOrNull() = (this as? Right<L, R>)?.value
+    fun asRightOrNull() = (this as? Right<L, R>)?.data
 
-    fun asLeft(): L = (this as Left<L, R>).value
+    fun asLeft(): L = (this as Left<L, R>).data
 
-    fun asRight(): R = (this as Right<L, R>).value
+    fun asRight(): R = (this as Right<L, R>).data
 
     inline fun <N> map(crossinline mapper: (R) -> N): Either<L, N> {
         return if (this.isLeft()) {
