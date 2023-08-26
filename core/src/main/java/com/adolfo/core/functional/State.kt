8b6f22a9dc6750b2e7379e80e4deb9b0c3aac1c6
Adolfo Santiago <epoch@adol.pw>
2023-08-26 15:57:18 +0200

    Deprecated notice for State class

--- core/src/main/java/com/adolfo/core/functional/State.kt
+++ core/src/main/java/com/adolfo/core/functional/State.kt
@@ -2,6 +2,7 @@ package com.adolfo.core.functional
 
 import com.adolfo.core.exception.Failure
 
+@Deprecated("Change for Either")
 sealed class State<out T> {
 
     data class Success<out T>(val data: T) : State<T>()
