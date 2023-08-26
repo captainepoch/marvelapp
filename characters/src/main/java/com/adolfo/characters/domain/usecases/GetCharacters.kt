6370b50b21864c8de311081ab86a950747490af5
Adolfo Santiago <epoch@adol.pw>
2023-08-26 15:57:30 +0200

    UseCase updated with Either instead of State

--- characters/src/main/java/com/adolfo/characters/domain/usecases/GetCharacters.kt
+++ characters/src/main/java/com/adolfo/characters/domain/usecases/GetCharacters.kt
@@ -1,17 +1,19 @@
 package com.adolfo.characters.domain.usecases
 
 import com.adolfo.characters.core.utils.CharactersConstants
+import com.adolfo.characters.data.models.data.CharactersData
 import com.adolfo.characters.data.models.view.CharactersView
 import com.adolfo.characters.domain.repository.CharactersRepository
+import com.adolfo.core.exception.Failure
 import com.adolfo.core.extensions.orFalse
-import com.adolfo.core.functional.State
+import com.adolfo.core.functional.Either
 import com.adolfo.core.interactor.UseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class GetCharacters(
     private val repository: CharactersRepository
-) : UseCase<GetCharacters.Params, State<CharactersView>>() {
+) : UseCase<GetCharacters.Params, Either<Failure, CharactersView>>() {
 
     override suspend fun execute(params: Params?) = withContext(Dispatchers.IO) {
         repository.getCharacters(
